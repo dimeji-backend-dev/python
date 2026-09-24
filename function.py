03549128a6cb0701a2a7750c088d610a1e695fef
akanni-oladimeji <dimejiakanni@gmail.com>
2026-09-24 10:58:33 +0100

    Refactor checks and requirement functions for improved structure and readability

--- function.py
+++ function.py
@@ -8,31 +8,30 @@ specials = list(string.punctuation)
 
 combo = number + small_letter + capital_letter + specials
 
+def checks(otp):
+    number_check = [num.isdigit() for num in otp]
+    capital_check = [char.isupper() for char in otp]
+    lower_check = [char.islower() for char in otp]
+    punct_check = [punct in string.punctuation for punct in otp]
+
+    return [number_check,capital_check,lower_check,punct_check]
+
+def requirement(requirements):
+    if (
+        requirements[0].count(True) >= 2
+        and requirements[1].count(True) >= 2
+        and requirements[2].count(True) >= 2
+        and requirements[3].count(True) >= 2
+    ):
+        return True
+    else:
+        return False
+
 for i in range(5):
     while True:
         otp = "".join(random.choices(combo, k=16))
-
-        def checks(otp):
-            number_check = [num.isdigit() for num in otp]
-            capital_check = [char.isupper() for char in otp]
-            lower_check = [char.islower() for char in otp]
-            punct_check = [punct in string.punctuation for punct in otp]
-
-            return [number_check,capital_check,lower_check,punct_check]
-
         requirements = checks(otp)
-
-        def requirement(requirements):
-            if (
-                requirements[0].count(True) >= 2
-                and requirements[1].count(True) >= 2
-                and requirements[2].count(True) >= 2
-                and requirements[3].count(True) >= 2
-            ):
-                return True
-            else:
-                return False
-
-        if requirement(requirements) == True:
+        result = requirement(requirements)
+        if result == True:
             print (otp)
             break
