9d8314de0d061ddb54277dd7f9a312271c26dcb3
akanni-oladimeji <dimejiakanni@gmail.com>
2026-09-24 09:19:50 +0100

    I was able to change the genarator in small pices of fuction, to avoid everything being cloged up.

---
+++ function.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+number = [str(num) for num in range(10)]
+small_letter = list(string.ascii_lowercase)
+capital_letter = list(string.ascii_uppercase)
+specials = list(string.punctuation)
+
+combo = number + small_letter + capital_letter + specials
+
+for i in range(5):
+    while True:
+        otp = "".join(random.choices(combo, k=16))
+
+        def checks(otp):
+            number_check = [num.isdigit() for num in otp]
+            capital_check = [char.isupper() for char in otp]
+            lower_check = [char.islower() for char in otp]
+            punct_check = [punct in string.punctuation for punct in otp]
+
+            return [number_check,capital_check,lower_check,punct_check]
+
+        requirements = checks(otp)
+
+        def requirement(requirements):
+            if (
+                requirements[0].count(True) >= 2
+                and requirements[1].count(True) >= 2
+                and requirements[2].count(True) >= 2
+                and requirements[3].count(True) >= 2
+            ):
+                print(otp)
+
+        requirement(requirements)
+        break
