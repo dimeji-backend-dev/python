7ca260d90fd473aa84bf4b749952a7840a165e27
akanni-oladimeji <dimejiakanni@gmail.com>
2026-09-25 13:48:27 +0100

    Fix typos in car names and clean up stock mapping logic

--- verify.py
+++ verify.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-cars = ["volvo","benz","toyota","bugatti","honda","wolkswagen","ferari","BYD"]
+cars = ["volvo","benz","toyota","bugatti","honda","Volkswagen","ferrari","BYD"]
 number = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 unique_rand = set()
@@ -11,6 +11,22 @@ while len(unique_rand) < len(cars):
 
 unique_id = list(unique_rand)
 
-stock = dict(zip(unique_id, cars))
+#stock = dict(zip(unique_id, cars))
 
-print(stock)
+stock = {}
+
+for i in range(len(cars)):
+    stock[unique_id[i]] = cars[i]
+
+#print(stock)
+
+# for car_id, car in stock.items():
+#     print(car_id,car)
+
+#print(stock)
+
+data = {'fnm8O4': 'volvo', 'PijEQi': 'benz', '3gtKGm': 'toyota', 'dmIxaf': 'bugatti', 'bHpi9j': 'honda', 'F1SaWl': 'Volkswagen', 'zlEDpp': 'ferrari', 'Np2ZgJ': 'BYD'}
+
+enquiries = input("provide the id of the car you wanted to make the complaint about: ")
+if enquiries in data.keys():
+    print(data[enquiries])
